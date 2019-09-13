544ca68637926565adb163e22c965de10b65c2c3
AbhinabSarkar <abhinab.sarkar@gmail.com>
2019-09-13 18:39:52 +0000

    Added namespace in SkypeForBusinessOnPremAdapter.cs

--- 3.SkypeForBusinessOnPremiseBot/src/SkypeConsoleBot/Implementations/Bot/SkypeForBusinessOnPremAdapter.cs
+++ 3.SkypeForBusinessOnPremiseBot/src/SkypeConsoleBot/Implementations/Bot/SkypeForBusinessOnPremAdapter.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SkypeConsoleBot.Implementations
 {
